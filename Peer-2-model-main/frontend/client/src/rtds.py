a5790c11dc6d520bcbc9df96e23af471fa126eac
Danny225 <devik.kota@gmail.com>
2024-05-24 12:44:36 +0530

    Update rtds.py

--- Peer-2-model-main/frontend/client/src/rtds.py
+++ Peer-2-model-main/frontend/client/src/rtds.py
@@ -15,15 +15,15 @@ s.connect((TCP_IP, TCP_PORT))
 
 start = time.clock()
 for i in range(1,101):
- f = float(i) + 0.15
- send_data_str = struct.pack('>if',i, f)
- s.send(send_data_str)
  recv_data_str = s.recv(BUFFER_SIZE)
  recv_data_str_unpacked = struct.unpack('>if',recv_data_str)
  j = int(recv_data_str_unpacked[0])
  t = float(recv_data_str_unpacked[1])
  print ("The j returned is: ", j)
  print ("The t returned is: ", t)
+ f = float(i) + 0.15
+ send_data_str = struct.pack('>if',i, f)
+ s.send(send_data_str)
  time.sleep(1) #This sleep is needed for the ClosePort() below
 
 s.close()
